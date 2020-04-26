367f8d6f5d3b57333256e8148ff8ee4bfa485512
dmitry_zozulia <z1dima1z@gmail.com>
2020-04-26 12:59:12 +0200

    First project on React

---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>Main content
+            <div>
+                <img src='https://filedn.com/ltOdFv1aqz1YIFhf4gTY8D7/ingus-info/BLOGS/Photography-stocks3/stock-photography-slider.jpg' />
+            </div>
+            <div>
+                avatar + descript
+            </div>
+            <div>
+                My post
+            <div>
+                    new post
+            </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div >
+    );
+}
+export default Profile;
